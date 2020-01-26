a3cd8d85806dff036cdb7a0f204a89b83ac69c6d
Dmitry Ibragimov <ibragimov.dp@gmail.com>
2020-01-26 13:53:21 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Dmitry on 26.01.2020.
+//  Copyright © 2020 Dmitry Ibragimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
