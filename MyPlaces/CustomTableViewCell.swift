119e8e55314bbec112d09147d4d836a3fe06e97a
Dmitry Ibragimov <ibragimov.dp@gmail.com>
2020-02-02 20:30:09 +0300

    Custom Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Dmitry on 02.02.2020.
+//  Copyright © 2020 Dmitry Ibragimov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+
+}
